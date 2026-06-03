d0cb84357a8a180711abd07c9b0641cfacbd8281
coyoteshkw <1944301179@qq.com>
2026-06-03 22:17:39 +0800

    feat: SEO google

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     config: {
-      darkMode: 'class'
-    }
+      darkMode: "class",
+    },
   },
   nitro: {
-    plugins: ['~/server/db/index.ts']
+    plugins: ["~/server/db/index.ts"],
   },
   runtimeConfig: {
-    sessionSecret: process.env.NUXT_SESSION_SECRET || 'dev-secret-change-in-production'
+    sessionSecret:
+      process.env.NUXT_SESSION_SECRET || "dev-secret-change-in-production",
   },
-  compatibilityDate: '2026-06-02'
-})
+  compatibilityDate: "2026-06-02",
+  app: {
+    head: {
+      meta: [
+        {
+          name: "google-site-verification",
+          content: "JcadAm97tmXoVw9woFKeKhL-p9SuC6YYJSteU37O7xM",
+        },
+      ],
+    },
+  },
+});
